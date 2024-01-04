467d8bd46f6c35ad0799b8f1065618111143734c
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-01-04 22:02:55 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppQuery from './AppQuery';
+import {data} from './data';
+import './App.css';
+export default function App() {
+  return(
+    <>
+    <div className='news-container'>
+    <h1 className='news-text'>ახალი ამბები</h1>
+      
+    </div>
+      <div className='info-container'>
+        {data.map((item) => {
+      return <AppQuery key={item.id} {...item} />
+        })}
+      </div>
+    </>
+  )
+}
